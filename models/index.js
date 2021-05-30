1ce411cef3ef1d17e590d562010b12aeb39d9634
Thor138 <tjcourey84@gmail.com>
2021-05-29 19:26:37 -0600

    most of the html layout is set and transfered in to the handlebars files

--- models/index.js
+++ models/index.js
@@ -1,3 +1,13 @@
 const User = require("./User");
+const Post = require("./Post");
 
-module.exports = { User };
+Post.belongsTo(User, {
+  foreignKey: "user_id",
+});
+
+User.hasMany(Post, {
+  foreignKey: "user_id",
+  onDelete: "CASCADE",
+});
+
+module.exports = { User, Post };
